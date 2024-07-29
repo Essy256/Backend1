b52378fd4622a873aa3ef4aa10ff664cc779ed92
Essy256 <acholaesther26@gmail.com>
2024-07-29 12:46:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,17 +13,18 @@ console.log(`server runing on port ${port}`)
 })
 
 require(dotenv/config)
+
 //function student(req,res){
 //console.log(req+res)
 
 //}
-
 
 //const student = (req,res)=>{console.log(req+res)}
 
 //(req,res)=>{console.log(req+res)}
 
 const mongoose = require('mongoose');
+
 //CONNECT TO THE DATABASE
 mongoose.connect(process.env.MONGODBLINK,)
     {useNewUrIParser:true}

0581f1ccc43ef634457466eea3ef083ae81f0d3c
Essy256 <acholaesther26@gmail.com>
2024-08-02 11:06:06 +0300

    added . routes

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require ('express');
 const mongoose = require('mongoose');
 require('dotenv/config')
 const routes = require ('./routes')
-
 const app = express();
-const port = 4000
-
-
 // MIDDLEWARE
 app.use('/api',routes)
 
--- routes.js
+++ routes.js
@@ -2,10 +2,21 @@ const express = require ('express')
 
 const router = express.Router()
                   //CRUD
-// GET METHOD
+// GET METHOD (all posts)
 router.get('/posts',(req,res)=>{
-    res.send('posts page')
-});
+
+    res.json(
+        {msg:'GET all post'}
+    )
+    
+})
+router.get('/posts/:id',(req,res)=>{
+    res.json(
+        {msg:'GET a specific post'}
+    )
+})
+
+              
               
 
 module.exports = router;
